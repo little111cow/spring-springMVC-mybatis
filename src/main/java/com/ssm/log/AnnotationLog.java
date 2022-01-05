85b100d6424a69b62cb36b87bc320147b527ab63
littlecow <whu_jianliu@163.com>
2022-01-05 18:22:28 +0800

    添加数据库文件

--- src/main/java/com/ssm/log/AnnotationLog.java
+++ src/main/java/com/ssm/log/AnnotationLog.java
@@ -7,12 +7,13 @@ import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class AnnotationLog {
+
     @Before("execution(* com.ssm.service.*.*(..))")
     public void before(){
-
+        System.out.println("方法执行前！");
     }
     @AfterReturning("execution(* com.ssm.service.*.*(..))")
     public void afterReturning(){
-
+        System.out.println("方法执行返回后！");
     }
 }
